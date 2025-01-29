12e009e4a59bcef9dd4c8844d1e7a24d7fe015a4
CalebChen768 <72332844+CalebChen768@users.noreply.github.com>
2025-01-29 10:26:02 +0100

    convert np.array to dataframe after column transformer

--- test_pipeline.py
+++ test_pipeline.py
@@ -6,14 +6,16 @@ from sklearn.pipeline import Pipeline
 from missingvalue import MissingValueChecker
 from outlier import OutlierHandler
 from alignTransformer import AlignTransformer
-from sklearn.preprocessing import Normalizer, OneHotEncoder, StandardScaler, MinMaxScaler
+from sklearn.preprocessing import Normalizer, OneHotEncoder, StandardScaler, MinMaxScaler, FunctionTransformer
 from MissDropper import MissDropper
 from outofbounds import OutOfBoundsChecker
+from functools import partial
+
 
 # Create test data
 df = pd.DataFrame({
-    "col 1": [1, 2, 4, np.nan, 5, 6, -7, 8, 9, 1000, 5], 
-    "col 2": ["cat", "dog", None, "cat", "dog", "dog", "cat", "dog", "cat", "dog", "mice"],
+    "col_1": [1, 2, 4, np.nan, 5, 6, -7, 8, 9, 1000, 5], 
+    "col_2": ["cat", "dog", None, "cat", "dog", "dog", "cat", "dog", "cat", "dog", "mice"],
 })
 
 # Create a pipeline
@@ -25,20 +27,57 @@ preprocessor = ColumnTransformer(
             ("outliers", OutlierHandler(strategy="clip")),
             ("normalizer", StandardScaler()),
             ("align_index", AlignTransformer(original_index=df.index)),
-        ]), ["col 1"]),
+        ]), ["col_1"]),
 
         ("cat", Pipeline([
             ("missing_values", MissingValueChecker(data_type="categorical", strategy="drop")),
             ("bound_constrain", OutOfBoundsChecker(allowed_set=["cat", "dog"])),
             ("align_index", AlignTransformer(original_index=df.index)),
-            ("onehot",OneHotEncoder(categories=[list(set(df[i].unique())-{None, np.nan}) for i in ["col 2"]], handle_unknown="ignore"))
-        ]), ["col 2"]),
+            ("onehot",OneHotEncoder(categories=[list(set(df[i].unique())-{None, np.nan}) for i in ["col_2"]], handle_unknown="ignore"))
+        ]), ["col_2"]),
     ],
     remainder="passthrough"
 )
 
+def to_dataframe(X, transformer, feature_names):
+    """
+    convert transformed data to DataFrame
+    """
+    transformed_feature_names = transformer.get_feature_names_out(feature_names)
+    return pd.DataFrame(X, columns=transformed_feature_names)
+
+def get_final_columns(column_transformer):
+    """
+    Iterate through ColumnTransformer to get the final transformed column names.
+    - If it is OneHotEncoder, calculate categories and get new column names
+    - For other transformers, keep the original column names
+    """
+    final_columns = []
+    # column_transformer.fit(df)
+    for name, pipeline, columns in column_transformer.transformers_:
+        isOnehot = False
+
+        for transformer in pipeline:
+            if isinstance(transformer, OneHotEncoder):
+                isOnehot = True
+        if isOnehot:
+            # OneHotEncoder
+            for col in columns:
+                unique_categories = [list(set(df[i].unique())-{None, np.nan}) for i in ["col_2"]][0]
+                print("unique_categories", unique_categories)
+                generated_col_names = [f"{col}_{category}" for category in unique_categories]
+                final_columns.extend(generated_col_names)
+        else:
+            # for other transformers, keep original column names
+            final_columns.extend(columns if isinstance(columns, list) else [columns])
+    
+    return final_columns
+
+
+
 pipeline = Pipeline([
     ("preprocessor", preprocessor),
+    ("to_df", FunctionTransformer(lambda X: pd.DataFrame(X, columns=get_final_columns(preprocessor)), validate=False)),
     ("dropper", MissDropper()),
 
 ])
